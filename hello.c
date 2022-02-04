2a4cb06ec1f3e6937c9da9ea156126d12957d8d8
xuefeng <2106919811@qq.com>
2022-02-04 21:53:49 +0800

    format

--- hello.c
+++ hello.c
@@ -5,32 +5,36 @@ int main(int argc, char **argv)
     int i, j, h;
     printf("输入一个奇数（不要太大哦）：");
     scanf("%d", &h);
-    if(h%2==0)
-    	h=h-1;
+    if (h % 2 == 0)
+        h = h - 1;
     for (i = 1; i <= h; i++)
     {
         if (i <= h / 2 + 1)
         {
-            for (j = 1; j <= h / 2 + 1 - i; j++){
-            	putchar(' ');
-            	putchar(' ');
-			}
-            for (j = 1; j <= 2 * i - 1; j++){
-            	putchar('*');
-            	putchar(' ');
-			}
+            for (j = 1; j <= h / 2 + 1 - i; j++)
+            {
+                putchar(' ');
+                putchar(' ');
+            }
+            for (j = 1; j <= 2 * i - 1; j++)
+            {
+                putchar('*');
+                putchar(' ');
+            }
             putchar('\n');
         }
         else
         {
-            for (j = 1; j <= i - h / 2 - 1; j++){
-            	putchar(' ');
-            	putchar(' ');
-			}
-            for (j = 1; j <= 2 * h - 2 * i + 1; j++){
-            	putchar('*');
-            	putchar(' ');
-			}
+            for (j = 1; j <= i - h / 2 - 1; j++)
+            {
+                putchar(' ');
+                putchar(' ');
+            }
+            for (j = 1; j <= 2 * h - 2 * i + 1; j++)
+            {
+                putchar('*');
+                putchar(' ');
+            }
             putchar('\n');
         }
     }
